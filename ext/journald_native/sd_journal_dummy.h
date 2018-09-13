9cf4d411622ef176d398b66a263cd055631f5170
Karli Sjoberg <karli@inparadise.se>
2018-09-13 10:05:32 +0200

    Fixed build on FreeBSD

--- ext/journald_native/sd_journal_dummy.h
+++ ext/journald_native/sd_journal_dummy.h
@@ -31,12 +31,12 @@
 #define LOG_INFO    6
 #define LOG_DEBUG   7
 
-/* iovec */
-struct iovec {
+/* dummy iovec */
+struct dummy_iovec {
     void  *iov_base;    /* Starting address */
     size_t iov_len;     /* Number of bytes to transfer */
 };
 
 inline int sd_journal_print(int priority, const char *format, ...)  { return 0; }
-inline int sd_journal_sendv(const struct iovec *iov, int n)         { return 0; }
+inline int sd_journal_sendv(const struct dummy_iovec *iov, int n)         { return 0; }
 inline int sd_journal_perror(const char *message)                   { return 0; }
